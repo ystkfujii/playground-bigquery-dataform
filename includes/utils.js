8f29f0b30cef24b469ce75f7fa1a7423f2ecc4ce
ystkfujii <ystk.fujii0731@gmail.com>
2026-02-09 15:20:31 +0000

    add replaceEnv

--- includes/utils.js
+++ includes/utils.js
@@ -1,10 +1,16 @@
 /**
- * 入力された値（カラム名や数値）に1を足す関数
+ * 入力された値（カラム名や数値）に1を足す文字列を返す
  */
 function addOne(value) {
-  return value + 1;
+  return `${value} + 1`;
+}
+
+function replaceEnv(text) {
+  if (!text) return "";
+  return `"${text.replaceAll("dev", "envdev")}"`;
 }
 
 module.exports = {
-  addOne
+  addOne,
+  replaceEnv
 };
